4480ec9daf87f3e83c8d66eb8191b9ca956495b5
zatrixsafe <490631401@qq.com>
2015-04-11 22:30:23 +0800

    update right code

--- 655/250.py
+++ 655/250.py
@@ -1,41 +1,16 @@
 class BichromeBoard:
 	def ableToDraw(self, board):
-		b = list(board)
-		l = []
-		ans = 0
-		for i in range(len(b)):
-			s = list(b[i])
-			for j in range(len(s)):
-				if s[j] == '?':
-					if j-1 >=0:
-						if s[j-1] == 'W':
-							s[j] = 'B'
-						else: s[j] = 'W'
-					elif i-1 >=0:
-						if b[i-1][j] == 'W':
-							s[j] = 'B'
-						else: s[j] = 'W'
-					elif j+1 < len(s):
-						if s[j+1] == 'W':
-							s[j] = 'B'
-						else: s[j] = 'W'
-					elif i+1 < len(b):
-						if b[i+1][j] == 'W':
-							s[j] = 'B'
-						else: s[j] = 'W'
-					else:
-						s[j] = 'W'
-				if j+1 < len(s):
-					if s[j] == s[j+1]:
-						ans = 1
-				if i+1 < len(b):
-					if s[j] == b[i+1][j]:
-						ans = 1
-		if ans == 0:
+		color1 = ['W', 'B']
+		color2 = ['B', 'W']
+		h ,w = len(board), len(board[0])
+
+		ret = all( board[i][j] in [color1[(i+j)%2], '?'] for i in range(h) for j in range(w) ) or all(
+				board[i][j] in [color2[(i+j)%2], '?'] for i in range(h) for j in range(w)) 
+
+		if ret: 
 			return 'Possible'
 		else:
 			return 'Impossible'
-		
 
 s = BichromeBoard()
 #x = ("W?W","??B","???")
