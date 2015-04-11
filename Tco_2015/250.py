db6a11df174ca8a390e050dc95696a35734c32ee
zatrixsafe <490631401@qq.com>
2015-04-12 01:47:46 +0800

    update tco2015 1a

---
+++ Tco_2015/250.py
@@ -0,0 +1,34 @@
+class Similars:
+	def getNum(self, x, y):
+		ans = 0
+		p = set(x)
+		q = set(y)
+		for i in p:
+			if i in q:
+				ans += 1
+		return ans
+	def getList(self, x):
+		l = []
+		while x > 0:
+			l.append(x%10)
+			x /= 10
+		l = sorted(l)
+		return l
+		
+	def maxsim(self, L, R):
+		f_ans = 0
+		ans = 0
+		l = []
+		for i in range(L, R+1):
+			t = self.getList(i)
+			l.append(t)
+		s = sorted(l)
+		for i in range(len(s)-1):
+			ans = self.getNum(s[i],s[i+1])
+			if f_ans < ans:
+				f_ans = ans
+		return f_ans
+x = Similars()
+L = 1
+R = 1234
+print x.maxsim(L,R)
